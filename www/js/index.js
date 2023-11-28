24acebd8f4e19a344a036dc1ea2bad5fcc9ab640
Blackhawk364 <2236001477@utna.edu.mx>
2023-11-27 19:24:31 -0600

    commit localStorage jr

--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -26,4 +6,60 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+}
+var Newuser = {};
+var AllUsers = [];  
+localStorage.setItem("Registros", AllUsers);
+var Registros =  localStorage.getItem("Registros");
+
+function Registrarse(){
+    var names = document.getElementById("names").value;
+    var lastnames = document.getElementById("lastnames").value;
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;
+    
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (names != "" && names != null && names != undefined  ){
+        if(lastnames != "" && lastnames != null && lastnames != undefined ){
+            if(email != "" && email != undefined && email.match(validRegex) ){
+                if(pass != "" && pass != null && pass != undefined   ){
+
+                    if(usuarioRepetido == false){
+                        Newuser = {
+                            "nombres" : names,
+                            "apellidos" : lastnames,
+                            "correo" : email,
+                            "contraseña": pass
+
+                        }
+                        AllUsers.push(Newuser);
+                        console.log(AllUsers);
+                        localStorage.setItem("Registros", AllUsers)
+                        alert("Usuario "+names +" registrado correctamente");
+                    }
+                    else{
+                        alert("Usuario ya registrado")
+                    }
+                }
+                else{
+                    alert("Escribe una contraseña valida");                    
+                }
+            }
+            else
+            {
+                alert("Escribe una correo valido");
+            }
+        }  
+        else{            
+            alert("Escribe un apellido valido");
+        }
+    }
+    else
+    {
+        alert("Escribe un nombre valido");
+    }
+}
+function usuarioRepetido(){
+
 }
